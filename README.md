e4bf2f2baefbead8b74a427df3f2b18b87331af8
André Barata <andre.quina.barata@gmai.com>
2024-11-29 08:28:14 +0000

    feat: adding readme, adding automount block

---
+++ README.md
@@ -0,0 +1,12 @@
+# PiBakery
+
+## Overview
+
+This project came about when I tried a failed to get https://github.com/davidferguson/pibakery on my computer due to its outdated dependencies.
+
+I went about updating them and extracting the parts of the project I needed finding several simplifications that were possible with recent libraries.
+
+The electron app has been split into two pieces, the designer UI and the builder cli.
+
+## Usage
+
---
+++ src/pibakery-blocks/automount/automount.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+apt-get install -y pmount
+
+echo 'ACTION=="add", KERNEL=="sd[a-z][0-9]", TAG+="systemd", ENV{SYSTEMD_WANTS}="usbstick-handler@%k"' > /etc/udev/rules.d/usbstick.rules
+
+echo "[Unit]
+Description=Mount USB sticks
+BindsTo=dev-%i.device
+After=dev-%i.device
+ 
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/local/bin/automount %I
+ExecStop=/usr/bin/pumount /dev/%I" > /lib/systemd/system/usbstick-handler@.service
+
+echo '#!/bin/bash
+ 
+PART=$1
+FS_LABEL=`lsblk -o name,label | grep ${PART} | awk '{print \$2}'`
+ 
+if [ -z ${FS_LABEL} ]
+then
+    /usr/bin/pmount --umask 000 --noatime -w --sync /dev/${PART} /media/${PART}
+else
+    /usr/bin/pmount --umask 000 --noatime -w --sync /dev/${PART} /media/${FS_LABEL}_${PART}
+fi' > /usr/local/bin/automount
+
+chmod +x /usr/local/bin/automount
